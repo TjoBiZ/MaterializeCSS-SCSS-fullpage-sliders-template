76d70ff5612c05b33a600aceb1d67e87ecc0650e
joker <joker@tjo.biz>
2019-08-25 20:25:31 +0700

    Add scr fullPage and delete console.log error Add option scrolloverflow source files for forms and sms Forms + callBack

--- language.js
+++ language.js
@@ -2,37 +2,25 @@ var arrLang = {
     'us': {
         'about': 'About Us',
         'descriptiongoal': 'Elite complex Andaman Rivera <br>\n' +
-            '                       We offer you apartment options ranging from 33.67 - 54.04 sq.m.\n' +
-            '                       Prices from 3.xx million Thai Baht, including high-quality finishes using expensive and high-quality materials.\n' +
-            '                       We also offer guaranteed income in the form of 24% for 3 years.\n' +
-            '                       The end of construction in the fall of 2020.',
+            '  We offer you apartment options ranging from 33.67 - 54.04 sq.m. Prices from 3.xx million Thai Baht, including high-quality finishes using expensive and high-quality materials. We also offer guaranteed income in the form of 24% for 3 years. The end of construction in the fall of 2020.',
         'btnaskquestion': 'Ask a Question'
     },
     'ru': {
         'about': 'О компании',
         'descriptiongoal': 'Элитный комплекс Andaman Rivera<br>\n' +
-            '                      Предлагаем вам варианты апартаментов площадью от 33.67 - 54.04 кв.м.\n' +
-            '                      Цены от 3.хх млн. Тайских Бат, включая высококачественную отделку с использованием дорогих и качественных материалов.\n' +
-            '                      Так же мы предлагаем гарантированный доход в виде 24% за 3 года.\n' +
-            '                      Окончание строительства осенью 2020 года.',
+            ' Предлагаем вам варианты апартаментов площадью от 33.67 - 54.04 кв.м. Цены от 3.хх млн. Тайских Бат, включая высококачественную отделку с использованием дорогих и качественных материалов. Так же мы предлагаем гарантированный доход в виде 24% за 3 года. Окончание строительства осенью 2020 года.',
         'btnaskquestion': 'Задать вопрос'
     },
     'ch': {
         'about': '關於我們',
         'descriptiongoal': '精英複雜安達曼里維拉<br>\n' +
-            '                       我們為您提供33.67  -  54.04平方米的公寓選擇。\n' +
-            '                       價格為3.xx百萬泰銖，包括使用昂貴和優質材料的高品質飾面。\n' +
-            '                       我們還提供24％的保證收入，為期3年。\n' +
-            '                       2020年秋季建設結束。',
+            '  我們為您提供33.67  -  54.04平方米的公寓選擇。  價格為3.xx百萬泰銖，包括使用昂貴和優質材料的高品質飾面。 我們還提供24％的保證收入，為期3年。 2020年秋季建設結束。',
         'btnaskquestion': '問一個問題'
     },
     'th': {
         'about': 'เกี่ยวกับเรา',
         'descriptiongoal': 'อีลิทคอมเพล็กซ์อันดามันริเวร่า <br>\n' +
-            '                       เราเสนอทางเลือกอพาร์ทเมนท์ให้คุณตั้งแต่ 33.67 - 54.04 ตร.ม.\n' +
-            '                       ราคาเริ่มต้น 3.xx ล้านบาทรวมทั้งงานคุณภาพสูงด้วยวัสดุราคาแพงและคุณภาพสูง\n' +
-            '                       เรายังเสนอรับประกันรายได้ในรูปแบบ 24% เป็นเวลา 3 ปี\n' +
-            '                       สิ้นสุดการก่อสร้างในฤดูใบไม้ร่วงปี 2020',
+            '  เราเสนอทางเลือกอพาร์ทเมนท์ให้คุณตั้งแต่ 33.67 - 54.04 ตร.ม. ราคาเริ่มต้น 3.xx ล้านบาทรวมทั้งงานคุณภาพสูงด้วยวัสดุราคาแพงและคุณภาพสูง เรายังเสนอรับประกันรายได้ในรูปแบบ 24% เป็นเวลา 3 ปี สิ้นสุดการก่อสร้างในฤดูใบไม้ร่วงปี 2020',
         'btnaskquestion': 'ถามคำถาม'
     }
 };
